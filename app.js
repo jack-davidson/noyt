384b07f7c6337647d4c82ef4e451efaaae164323
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-05-04 16:46:48 -0500

    update deps

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+const port = 3000;
+const hostname = 'localhost';
+
+app.use(express.static('/public'));
+
+app.get('/', (req, res) => {
+    res.send('/');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`app.js listening on http://${hostname}:${port}`);
+});
