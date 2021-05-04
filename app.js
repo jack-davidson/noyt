85a7935c28d18de4bd911d21dbf5cbe99ce00c67
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-05-04 18:10:23 -0500

    add

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
-const ejs = require('ejs');
+const youtubeDl = require('youtube-dl-exec');
+const fs = require('fs');
 
 const app = express();
 
 const port = 3000;
 const hostname = 'localhost';
 
-app.use(express.static('/public'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.send('/');
+app.set('views', 'templates');
+app.set('view engine', 'ejs');
+
+app.get('/', (_, res) => {
+    res.render('index.ejs', {});
+});
+
+app.post('/video', (req, res) => {
+    youtubeDl(req.body.video_url, {
+        dumpJson: true,
+        noCallHome: true,
+        format: 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4'
+    }).then((output) => {
+        res.send(`
+        <a href=\"${'public/videos/' + output['title'] + '.' + output['ext']}\" download>click here to download your video</a>
+        `)
+    });
 });
 
 app.listen(port, hostname, () => {
